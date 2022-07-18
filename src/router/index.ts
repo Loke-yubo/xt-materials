fb593fb6423c6ac8aa6feba1ad7477ebac486549
王东斌 <dongbin.wang@xtransfer.cn>
2022-07-18 10:56:32 +0800

    feat: 完善一下文档方案，然后新增test路由及页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,24 +2,32 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes: Array<RouteRecordRaw> = [
-    {
-        path: '/',
-        name: 'home',
-        component: HomeView
-    },
-    {
-        path: '/about',
-        name: 'about',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    }
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/page1-test',
+    name: 'page1-test',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views-test/page1-test.vue')
+  }
 ]
 const base = window.__POWERED_BY_QIANKUN__ ? '/boss2/xt-materials' : process.env.BASE_URL
 const router = createRouter({
-    history: createWebHistory(base),
-    routes
+  history: createWebHistory(base),
+  routes
 })
 
 export default router
